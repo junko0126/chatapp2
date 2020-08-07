7e7bc911bc8bce5c18db7267e11fdeefb319918c
junko0126 <hikari2011h.j@gmail.com>
2020-08-07 13:06:42 +0900

    メッセージの表示

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,4 @@
 class Message < ApplicationRecord
-  def change
-    create_table :messages do |t|
-      t.string :content
-      t.references :room, foreign_key: true
-      t.references :user, foreign_key: true
-      t.timestamps
-    end
     belongs_to :user
     belongs_to :room
-
-  end
 end
